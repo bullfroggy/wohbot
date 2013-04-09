f856b5deb2ab7f6ea17ffe01a6e595481b98b776
Christopher Swenor <cswenor@gmail.com>
2013-04-09 11:17:03 -0400

    Added an interface for whiney Rob

--- farm_newest.py
+++ farm_newest.py
@@ -4,6 +4,20 @@ from bot import Bot
 from woh import WoH
 
 
+def run(sid):
+    player_settings = {
+        "sid": sid,
+    }
+    player = Player(player_settings)
+
+    bot_settings = {
+        "player": player,
+    }
+    bot = Bot(bot_settings)
+
+    bot.max_farm_newest_mission()
+
+
 def main():
     p = optparse.OptionParser()
     p.add_option('--sid', '-s', default="")
@@ -12,20 +26,8 @@ def main():
     if not options.sid:
         print "Please enter your SID:"
         options.sid = raw_input()
-
-    player_settings = {
-        "sid": options.sid,
-
-    }
-    player = Player(player_settings)
-    bot_settings = {
-        "player": player,
-    }
-    bot = Bot(bot_settings)
-
-    bot.max_farm_newest_mission()
-
-    print "Done!"
+    
+    run(options.sid)
 
 if __name__ == '__main__':
     main()
